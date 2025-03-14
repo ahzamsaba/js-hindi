f77858424ff379bdb5e7128fb20b1d8f38b20dfd
Ahzam Saba <156760922+ahzamsaba@users.noreply.github.com>
2025-03-14 11:05:20 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,5 +27,42 @@ let boolIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+// *******************Operations*******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);//power
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " Ahzam"
+let str3 = str1 + str2
+// console.log(str3);//hello Ahzam
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2");//32
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+// ++gameCounter
+// console.log(gameCounter++);//100
+// console.log(++gameCounter);//102
