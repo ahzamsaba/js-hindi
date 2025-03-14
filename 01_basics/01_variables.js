dacc11097c9d124aaab391cfc201c2a3b62c0025
Ahzam Saba <156760922+ahzamsaba@users.noreply.github.com>
2025-03-14 10:03:32 +0000

    const_let_table

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 62037
+let accountEmail = "ahzam@gmail.com"
+var accountPassword = "12345"
+accountCity = "Muzaffarpur"
+
+// accountId = 2
+
+accountEmail = "saba@gmail.com"
+accountPassword = "54321"
+accountCity = "Gaya"
+
+let accountState;
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+NOTE -> Prefer not to use var
+        because of issue in block scope & functional scope
+*/
