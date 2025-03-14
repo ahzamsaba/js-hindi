240f7620bd52c81f4bc36c1c08a73410ac7a3b73
Ahzam Saba <156760922+ahzamsaba@users.noreply.github.com>
2025-03-14 10:30:43 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";//treta all JS code as newer version
+
+// alert(5 * 2)   we are using nodejs , not browser
+
+let name = "Ahzam"
+let age = 23
+let isLoggedIn = true
+
+
+// number
+// bigint
+// string => "" or ''
+// boolean
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "Ahzam");//string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
