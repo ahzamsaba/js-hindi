7254c43cbbe6f0d5d1fd9f327bebfea786db3cc4
ahzamsaba <ahzamsaba3052@gmail.com>
2025-04-24 20:41:32 +0530

    localStorage (RockPaperScissor)

--- 13_miniProjects/05_rock_paper_scissor/index.js
+++ 13_miniProjects/05_rock_paper_scissor/index.js
@@ -2,10 +2,13 @@ const playerText = document.querySelector('#playerText')
 const computerText = document.querySelector('#computerText')
 const resultText = document.querySelector('#resultText')
 
-const choiceBtns = document.querySelectorAll('.choiceBtn')
+const choiceBtns = document.querySelectorAll('.choiceBtn:not(#reset)')
 let player
 let computer
 let result
+let playerScore = parseInt(localStorage.getItem('playerScore')) || 0
+let computerScore = parseInt(localStorage.getItem('computerScore')) || 0
+updateScoreBoard()
 
 choiceBtns.forEach(button => button.addEventListener('click', () => {
     button.classList.add('btn-animate')
@@ -18,6 +21,14 @@ choiceBtns.forEach(button => button.addEventListener('click', () => {
     playerText.textContent = `Player : ${player}`
     computerText.textContent = `Computer : ${computer}`
     resultText.textContent = checkWinner()
+    if(resultText.textContent == 'You Win!'){
+        playerScore++
+        saveScore()
+    }
+    else if(resultText.textContent == 'You Lose!'){
+        computerScore++
+        saveScore()
+    }
 }))
 
 function computerTurn(){
@@ -48,4 +59,20 @@ function checkWinner(){
     else if(computer == 'SCISSOR'){
         return (player == 'ROCK' ? 'You Win!' : 'You Lose!')
     }
+}
+
+function saveScore(){
+    localStorage.setItem('playerScore',playerScore)
+    localStorage.setItem('computerScore',computerScore)
+    updateScoreBoard()
+}
+
+function updateScoreBoard(){
+    document.getElementById('scoreBoard').textContent = `Score - Player : ${playerScore} | Computer : ${computerScore}`
+}
+
+function resetScore(){
+    playerScore = 0
+    computerScore = 0
+    saveScore()
 }
